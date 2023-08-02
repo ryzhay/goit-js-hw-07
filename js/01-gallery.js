e10008c5ae28c2f6b0be96aa80c6af5854174635
ryzhay <elenaaltukhova1@gmail.com>
2023-08-02 21:35:49 +0200

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const itemsMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup);
+gallery.addEventListener('click', onItemClick);
+gallery.addEventListener('keydown', onEscClose);
+
+function createMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+          <img
+            class='gallery__image'
+            src='${preview}'
+            data-source='${original}'
+            alt='${description}'
+          />
+        </a>
+      </li>`;
+    }).join('');
+};
+
+function onItemClick(event) {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}' width="800" height="600">
+`);
+instance.show();
+
+};
+
+function onEscClose(event){
+    if(event.code === 'Escape') {
+        instance.close();
+    }
+    };
+console.log(galleryItems);
